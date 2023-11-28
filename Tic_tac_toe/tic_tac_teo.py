83c7ef4edf66e67fa6a9dbdd6a33c02cfcd991b7
Minji Jung <minji.jung@developersinstitute.co.nz>
2023-11-29 02:04:05 +1300

    Add comments about how it works/function

--- Tic_tac_toe/tic_tac_teo.py
+++ Tic_tac_toe/tic_tac_teo.py
@@ -1,5 +1,10 @@
+# Game board representation
 squares = [" "] * 9
+
+# Players: 'X' and 'O'
 players = "XO"
+
+# Board display template
 board = """
   0   1   2
   {0} | {1} | {2}
@@ -9,6 +14,8 @@ board = """
   {6} | {7} | {8}
   6   7   8
 """
+
+# Winning conditions for the game
 win_conditions = [
     (0, 1, 2),
     (3, 4, 5),
@@ -21,22 +28,35 @@ win_conditions = [
 ]
 
 
+# Function to check if a player has won
 def check_win(player):
     for a, b, c in win_conditions:
         if {squares[a], squares[b], squares[c]} == {player}:
             return True
 
 
+# Main game loop
 while True:
+    # Display the current state of the board
     print(board.format(*squares))
+
+    # Check if 'O' has won
     if check_win(players[1]):
         print(f"{players[1]} is the winner!")
         break
+
+    # Check for a tie (Cats game)
     if " " not in squares:
         print("Cats game!")
         break
+
+    # Player move input
     move = input(f"{players[0]} to move [0-8] > ")
+
+    # Validate the move input
     if not move.isdigit() or not 0 <= int(move) <= 8 or squares[int(move)] != " ":
         print("Invalid move!")
         continue
+
+    # Update the board and switch players
     squares[int(move)], players = players[0], players[::-1]
