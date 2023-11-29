7b5d1ab1db67382414c6594f5e519bb56e8affed
Minji Jung <minji.jung@developersinstitute.co.nz>
2023-11-29 14:06:10 +1300

    Update related player moves

--- Tic_tac_toe/tic_tac_teo.py
+++ Tic_tac_toe/tic_tac_teo.py
@@ -33,12 +33,29 @@ def check_win(player):
     for a, b, c in win_conditions:
         if {squares[a], squares[b], squares[c]} == {player}:
             return True
+    return False
+
+
+# Function to display the current state of the game board
+def display_board():
+    print(board.format(*squares))
+
+
+# Function to get a valid player move
+def get_player_move():
+    move = input(f"{players[0]} to move [0-8] > ")
+    return (
+        int(move)
+        if move.isdigit() and 0 <= int(move) <= 8 and squares[int(move)] == " "
+        else None
+    )
 
 
 # Main game loop
 while True:
     # Display the current state of the board
-    print(board.format(*squares))
+    # print(board.format(*squares))
+    display_board()
 
     # Check if 'O' has won
     if check_win(players[1]):
@@ -54,9 +71,18 @@ while True:
     move = input(f"{players[0]} to move [0-8] > ")
 
     # Validate the move input
-    if not move.isdigit() or not 0 <= int(move) <= 8 or squares[int(move)] != " ":
+    # if not move.isdigit() or not 0 <= int(move) <= 8 or squares[int(move)] != " ":
+    #     print("Invalid move!")
+    #     continue
+    if move is None:
         print("Invalid move!")
         continue
 
     # Update the board and switch players
-    squares[int(move)], players = players[0], players[::-1]
+    # squares[int(move)], players = players[0], players[::-1]
+
+    # Update the square based on the player's move
+    squares[int(move)] = players[0]
+
+    # Switch players for the next move
+    players = players[::-1]
